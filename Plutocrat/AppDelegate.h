5b960fb1f6e6410b2394f8111a12a3b8f7b2f25f
Pavel Dolgov <xtasy064@gmail.com>
2016-05-02 14:36:51 +0300

    Initial Commit

---
+++ Plutocrat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Plutocrat
+//
+//  Created by Pavel Dolgov on 16-05-02.
+//  Copyright © 2016 Whitefly Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Plutocrat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Plutocrat
+//
+//  Created by Pavel Dolgov on 16-05-02.
+//  Copyright © 2016 Whitefly Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
